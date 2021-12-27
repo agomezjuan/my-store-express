f28db70b13c650c996cdd32d3d2a9ae957c864c0
Alberto Gomez Juan <agomezjuan@hotmail.com>
2021-12-27 18:42:20 +0100

    Primer acercamiento a las rutas de Express

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Prueba de conexion My App");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// RUTAS
+// Ruta raiz
+app.get('/', (req, res) => {
+  res.send('Hola mi servidor Express');
+});
+
+// Ruta de prueba
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Este es mi nuevo endpoint');
+});
+
+app.get('/otra-ruta', (req, res) => {
+  res.send('Otra ruta');
+});
+
+app.get('/json', (req, res) => {
+  res.json({
+    producto: 'Arduino MKR 1330',
+    precio: 60.0,
+  });
+});
+
+app.listen(port, () => {
+  console.log('Mi port ' + port);
+});
